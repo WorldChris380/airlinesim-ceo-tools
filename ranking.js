e89606b89189e52fb4d49dedb26405e7e3de30e1
Christian Böhme <121933301+WorldChris380@users.noreply.github.com>
2025-04-22 07:51:19 +0200

    Added continent ranking

--- ranking.js
+++ ranking.js
@@ -16,25 +16,41 @@ if (
         <div>
           <table class="table table-striped table-hover">
             <tbody>
+            <thead>
+              <th style="vertical-align: middle;">Ranking (PAX per week)</th>
+              <th style="vertical-align: middle;">Position</th>
+              <th style="vertical-align: middle; text-align: right;">Update</th>
+              <th style="vertical-align: middle; text-align: right;">Last Update</th>
+            </thead>
               <tr>
-                <th style="vertical-align: middle;">Country ranking (Pax/week)</th>
+                <th style="vertical-align: middle;">Country</th>
                 <td id="country-ranking-position" style="vertical-align: middle;">Not checked yet.</td>
                 <td style="vertical-align: middle; text-align: right;">
                   <a id="update-country-ranking-link">
-                    <button class="btn btn-default" id="update-country-ranking-button">Update Countryrank</button>
+                    <button class="btn btn-default" id="update-country-ranking-button">Update</button>
                   </a>
                 </td>
                 <td id="last-country-update-date" style="vertical-align: middle; text-align: right;">Not updated yet</td>
               </tr>
               <tr>
-                <th style="vertical-align: middle;">World ranking (Pax/week)</th>
+                <th style="vertical-align: middle;">Continent</th>
                 <td id="ranking-position" style="vertical-align: middle;">Not checked yet.</td>
                 <td style="vertical-align: middle; text-align: right;">
-                  <a id="update-ranking-link">
-                    <button class="btn btn-default" id="update-world-ranking-button">Update Worldrank</button>
+                  <a id="update-contintent-ranking-link">
+                    <button class="btn btn-default" id="update-contintent-ranking-button">Update</button>
                   </a>
                 </td>
                 <td id="last-update-date" style="vertical-align: middle; text-align: right;">Not updated yet</td>
+              </tr>
+              <tr>
+                <th style="vertical-align: middle;">World</th>
+                <td id="world-ranking-position" style="vertical-align: middle;">Not checked yet.</td>
+                <td style="vertical-align: middle; text-align: right;">
+                  <a id="update-ranking-link">
+                    <button class="btn btn-default" id="update-world-ranking-button">Update</button>
+                  </a>
+                </td>
+                <td id="last-world-update-date" style="vertical-align: middle; text-align: right;">Not updated yet</td>
               </tr>
             </tbody>
           </table>
@@ -49,9 +65,6 @@ if (
   // Abrufen des gespeicherten Rankings aus dem localStorage
   let savedRanking = localStorage.getItem("rankingPosition");
   let rankingPositionTd = document.getElementById("ranking-position");
-  let updateRankingButton = document.getElementById(
-    "update-world-ranking-button"
-  );
   if (savedRanking) {
     console.log("Gespeichertes Ranking:", savedRanking);
     rankingPositionTd.innerHTML = "#" + savedRanking;
@@ -66,23 +79,42 @@ if (
   let airlineName = getAirlineName.innerText;
 
   // Event-Handler für den Update-World-Ranking-Button
-  let lastUpdateDateTd = document.getElementById("last-update-date");
+  let updateRankingButton = document.getElementById(
+    "update-world-ranking-button"
+  );
+  let worldRankingPositionTd = document.getElementById(
+    "world-ranking-position"
+  );
+  let lastWorldUpdateDateTd = document.getElementById("last-world-update-date");
+
   updateRankingButton.onclick = function () {
     // Aktualisiere das World Ranking
-    checkRanking(airlineName, rankingPositionTd);
+    checkRanking(airlineName, worldRankingPositionTd);
 
     // Setze das Datum des letzten Updates
     let currentDate = new Date().toLocaleString();
-    lastUpdateDateTd.innerText = currentDate;
+    lastWorldUpdateDateTd.innerText = currentDate;
 
-    // Speichere das Datum im localStorage
-    localStorage.setItem("lastUpdateDate", currentDate);
+    // Speichere das Datum im localStorage mit Subdomain
+    let subdomain = getSubdomain();
+    localStorage.setItem(`lastWorldUpdateDate_${subdomain}`, currentDate);
   };
 
+  // Setze das gespeicherte World Ranking beim Laden der Seite
+  let subdomain = getSubdomain();
+  let savedWorldRanking = localStorage.getItem(
+    `worldRankingPosition_${subdomain}`
+  );
+  if (savedWorldRanking) {
+    worldRankingPositionTd.innerHTML = "#" + savedWorldRanking;
+  }
+
   // Setze das gespeicherte Datum beim Laden der Seite
-  let lastUpdateDate = localStorage.getItem("lastUpdateDate");
-  if (lastUpdateDate) {
-    lastUpdateDateTd.innerText = lastUpdateDate;
+  let lastWorldUpdateDate = localStorage.getItem(
+    `lastWorldUpdateDate_${subdomain}`
+  );
+  if (lastWorldUpdateDate) {
+    lastWorldUpdateDateTd.innerText = lastWorldUpdateDate;
   }
 
   // Event-Handler für den Update-Country-Ranking-Button
@@ -118,39 +150,149 @@ if (
   if (lastCountryUpdateDate) {
     lastCountryUpdateDateTd.innerText = lastCountryUpdateDate;
   }
+
+  // Event-Handler für den Update-Continent-Ranking-Button
+  let updateContinentRankingButton = document.getElementById(
+    "update-contintent-ranking-button"
+  );
+  let continentRankingPositionTd = document.getElementById("ranking-position");
+  let lastContinentUpdateDateTd = document.getElementById("last-update-date");
+
+  updateContinentRankingButton.onclick = function () {
+    // Hole die Continent-ID aus der Seite
+    let continentIDElement = document.querySelector(
+      "#enterprise-dashboard > div:nth-child(1) > div.as-panel.facts > div > table > tbody > tr:nth-child(3) > td > a"
+    );
+
+    if (!continentIDElement) {
+      console.log("Continent-ID konnte nicht gefunden werden.");
+      return;
+    }
+
+    // Öffne den Link und finde die Continent-ID
+    let continentHref = continentIDElement.href;
+    let newWindow = window.open(continentHref);
+
+    newWindow.onload = function () {
+      let continentNameElement = newWindow.document.querySelector(
+        "body > div.container-fluid > div > div > div > div:nth-child(1) > div.col-md-4 > div > div > table > tbody:nth-child(1) > tr:nth-child(9) > td:nth-child(2)"
+      );
+
+      if (!continentNameElement) {
+        console.log("Continent-Name konnte nicht gefunden werden.");
+        newWindow.close();
+        return;
+      }
+
+      let continentName = continentNameElement.innerText.trim();
+      console.log("Gefundener Kontinent:", continentName);
+
+      // Mappe den Kontinentnamen auf die ID
+      let continentIDMap = {
+        Afrika: 1,
+        Europa: 2,
+        Mittelamerika: 3,
+        Nordamerika: 4,
+        Südamerika: 5,
+        Ostasien: 6,
+        Vorderasien: 7,
+        Ozeanien: 8,
+      };
+
+      let continentID = continentIDMap[continentName];
+      if (!continentID) {
+        console.log("Ungültiger Kontinent:", continentName);
+        newWindow.close();
+        return;
+      }
+
+      console.log("Gefundene Continent-ID:", continentID);
+
+      // Rufe das Continent-Ranking ab
+      checkContinentRanking(
+        airlineName,
+        continentRankingPositionTd,
+        continentID
+      );
+
+      // Setze das Datum des letzten Updates
+      let currentDate = new Date().toLocaleString();
+      lastContinentUpdateDateTd.innerText = currentDate;
+
+      // Speichere das Datum im localStorage
+      localStorage.setItem("lastContinentUpdateDate", currentDate);
+
+      newWindow.close();
+    };
+  };
+
+  // Setze das gespeicherte Continent Ranking beim Laden der Seite
+  let savedContinentRanking = localStorage.getItem("continentRankingPosition");
+  if (savedContinentRanking) {
+    continentRankingPositionTd.innerHTML = "#" + savedContinentRanking;
+  }
+
+  // Setze das gespeicherte Datum beim Laden der Seite
+  let lastContinentUpdateDate = localStorage.getItem("lastContinentUpdateDate");
+  if (lastContinentUpdateDate) {
+    lastContinentUpdateDateTd.innerText = lastContinentUpdateDate;
+  }
 }
 
 // Funktion zum Abrufen des Rankings
-function checkRanking(airlineName, rankingPositionTd) {
+function checkRanking(airlineName, worldRankingPositionTd) {
   let host = window.location.host;
   let parts = host.split(".");
-  let subdomain = parts.length > 2 ? parts.slice(0, -2).join(".") : "";
-  let statisticURL = `https://${subdomain}.airlinesim.aero/action/info/stat?type=transrecpax&country=0`;
+  let subdomain = getSubdomain();
 
-  let newWindow = window.open(statisticURL);
-  newWindow.onload = function () {
-    let foundElement = Array.from(
-      newWindow.document.querySelectorAll("td")
-    ).find((td) => td.innerText.trim() === airlineName);
+  // URLs, die vor dem Abrufen des World-Rankings aufgerufen werden müssen
+  let continentZeroURL = `https://${subdomain}.airlinesim.aero/action/info/stat?type=transrecpax&continent=0`;
+  let countryZeroURL = `https://${subdomain}.airlinesim.aero/action/info/stat?type=transrecpax&country=0`;
 
-    if (foundElement) {
-      let previousSibling = foundElement.previousElementSibling;
+  // Öffne die erste URL (continent=0)
+  let continentZeroWindow = window.open(continentZeroURL);
+  continentZeroWindow.onload = function () {
+    console.log("Continent=0 Statistik geladen.");
+    continentZeroWindow.close();
 
-      if (previousSibling) {
-        console.log("World Ranking gefunden:", previousSibling.innerText);
-        rankingPositionTd.innerHTML = "#" + previousSibling.innerText;
+    // Öffne die zweite URL (country=0) nach dem Schließen der ersten
+    let countryZeroWindow = window.open(countryZeroURL);
+    countryZeroWindow.onload = function () {
+      console.log("Country=0 Statistik geladen.");
+      countryZeroWindow.close();
 
-        // Speichere den Wert im localStorage
-        localStorage.setItem("rankingPosition", previousSibling.innerText);
-      } else {
-        let noDataMessage =
-          "Airline/Server just started - no Position available yet.";
-        rankingPositionTd.innerHTML = noDataMessage;
-      }
-    } else {
-      console.log("Airline wurde im World Ranking nicht gefunden.");
-    }
-    newWindow.close();
+      // Nach dem Schließen der zweiten URL, rufe das World-Ranking ab
+      let statisticURL = `https://${subdomain}.airlinesim.aero/action/info/stat?type=transrecpax&limit=0`;
+
+      let newWindow = window.open(statisticURL);
+      newWindow.onload = function () {
+        let foundElement = Array.from(
+          newWindow.document.querySelectorAll("td")
+        ).find((td) => td.innerText.trim() === airlineName);
+
+        if (foundElement) {
+          let previousSibling = foundElement.previousElementSibling;
+
+          if (previousSibling) {
+            console.log("World Ranking gefunden:", previousSibling.innerText);
+            worldRankingPositionTd.innerHTML = "#" + previousSibling.innerText;
+
+            // Speichere den Wert im localStorage mit Subdomain
+            localStorage.setItem(
+              `worldRankingPosition_${subdomain}`,
+              previousSibling.innerText
+            );
+          } else {
+            let noDataMessage =
+              "Airline/Server just started - no Position available yet.";
+            worldRankingPositionTd.innerHTML = noDataMessage;
+          }
+        } else {
+          console.log("Airline wurde im World Ranking nicht gefunden.");
+        }
+        newWindow.close();
+      };
+    };
   };
 }
 
@@ -160,19 +302,73 @@ function checkCountryRanking(airlineName, countryRankingPositionTd) {
   let parts = host.split(".");
   let subdomain = parts.length > 2 ? parts.slice(0, -2).join(".") : "";
 
-  // Hole die Country-ID aus der Seite
-  let countryIDElement = document.querySelector(
-    "#enterprise-dashboard > div:nth-child(1) > div.as-panel.facts > div > table > tbody > tr:nth-child(4) > td > a"
-  );
-  if (!countryIDElement) {
-    console.log("Country ID konnte nicht gefunden werden.");
-    return;
-  }
-  let countryID = countryIDElement.href.split("=").pop();
-  console.log("Country ID:", countryID);
+  // URL für das Continent-0-Statistikfenster
+  let continentZeroURL = `https://${subdomain}.airlinesim.aero/action/info/stat?type=transrecpax&continent=0`;
 
-  // URL für das Country Ranking
-  let statisticURL = `https://${subdomain}.airlinesim.aero/action/info/stat?type=transrecpaxcap&country=${countryID}&limit=0`;
+  // Öffne das Continent-0-Statistikfenster
+  let continentZeroWindow = window.open(continentZeroURL);
+
+  continentZeroWindow.onload = function () {
+    console.log("Continent=0 Statistik geladen.");
+    continentZeroWindow.close();
+
+    // Nach dem Schließen des Continent-0-Fensters, hole die Country-ID
+    let countryIDElement = document.querySelector(
+      "#enterprise-dashboard > div:nth-child(1) > div.as-panel.facts > div > table > tbody > tr:nth-child(4) > td > a"
+    );
+    if (!countryIDElement) {
+      console.log("Country ID konnte nicht gefunden werden.");
+      return;
+    }
+    let countryID = countryIDElement.href.split("=").pop();
+    console.log("Country ID:", countryID);
+
+    // URL für das Country Ranking
+    let statisticURL = `https://${subdomain}.airlinesim.aero/action/info/stat?type=transrecpax&country=${countryID}&limit=0`;
+
+    let newWindow = window.open(statisticURL);
+    newWindow.onload = function () {
+      let foundElement = Array.from(
+        newWindow.document.querySelectorAll("td")
+      ).find((td) => td.innerText.trim() === airlineName);
+
+      if (foundElement) {
+        let previousSibling = foundElement.previousElementSibling;
+
+        if (previousSibling) {
+          console.log("Country Ranking gefunden:", previousSibling.innerText);
+          countryRankingPositionTd.innerHTML = "#" + previousSibling.innerText;
+
+          // Speichere das Country Ranking im localStorage
+          localStorage.setItem(
+            "countryRankingPosition",
+            previousSibling.innerText
+          );
+        } else {
+          let noDataMessage =
+            "Airline/Server just started - no Country Position available yet.";
+          countryRankingPositionTd.innerHTML = noDataMessage;
+        }
+      } else {
+        console.log("Airline wurde im Country Ranking nicht gefunden.");
+      }
+      newWindow.close();
+    };
+  };
+}
+
+// Funktion zum Abrufen des Continent Rankings
+function checkContinentRanking(
+  airlineName,
+  continentRankingPositionTd,
+  continentID
+) {
+  let host = window.location.host;
+  let parts = host.split(".");
+  let subdomain = parts.length > 2 ? parts.slice(0, -2).join(".") : "";
+
+  // URL für das Continent Ranking
+  let statisticURL = `https://${subdomain}.airlinesim.aero/action/info/stat?type=transrecpax&continent=${continentID}`;
 
   let newWindow = window.open(statisticURL);
   newWindow.onload = function () {
@@ -184,22 +380,29 @@ function checkCountryRanking(airlineName, countryRankingPositionTd) {
       let previousSibling = foundElement.previousElementSibling;
 
       if (previousSibling) {
-        console.log("Country Ranking gefunden:", previousSibling.innerText);
-        countryRankingPositionTd.innerHTML = "#" + previousSibling.innerText;
+        console.log("Continent Ranking gefunden:", previousSibling.innerText);
+        continentRankingPositionTd.innerHTML = "#" + previousSibling.innerText;
 
-        // Speichere das Country Ranking im localStorage
+        // Speichere das Continent Ranking im localStorage
         localStorage.setItem(
-          "countryRankingPosition",
+          "continentRankingPosition",
           previousSibling.innerText
         );
       } else {
         let noDataMessage =
-          "Airline/Server just started - no Country Position available yet.";
-        countryRankingPositionTd.innerHTML = noDataMessage;
+          "Airline/Server just started - no Continent Position available yet.";
+        continentRankingPositionTd.innerHTML = noDataMessage;
       }
     } else {
-      console.log("Airline wurde im Country Ranking nicht gefunden.");
+      console.log("Airline wurde im Continent Ranking nicht gefunden.");
     }
     newWindow.close();
   };
+}
+
+// Funktion zum Abrufen der Subdomain
+function getSubdomain() {
+  let host = window.location.host;
+  let parts = host.split(".");
+  return parts.length > 2 ? parts.slice(0, -2).join(".") : "default";
 }
